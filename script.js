f6d28d862b302b3cdf7fe09387b395c898485d59
Gyromitte <gyromitte@gmail.com>
2021-10-05 17:09:39 -0500

    Created a function that  stores new books into an array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -8,5 +10,18 @@ function Book(title, author, pages, read){
         return info;
     }
 }
-const cleanCode = new Book("clean code", "someone", 69, true);
-console.log(cleanCode.info());
+
+function addBookToLibrary(){
+    title = prompt("Write the title of the book: ");
+    author = prompt("Write the author of the book: ");
+    pages = prompt("Write how many pages does the book have: ");
+    read = prompt("Have you read this book yet?: ");
+    title = new Book(title, author, pages, read);
+    myLibrary.push(title);
+}
+addBookToLibrary();
+console.log(title.info());
+console.log(myLibrary[0]);
+addBookToLibrary();
+console.log(myLibrary[1]);
+
