b8a3e062a669a0f7c455fa355c691081b71e6aa3
Gyromitte <gyromitte@gmail.com>
2021-10-05 19:00:11 -0500

    Tested new code to create game cards trough javaScript

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read){
+const library = document.getElementById('library');
+
+const gameCard = document.createElement('div');
+gameCard.classList.add('game-card');
+
+library.appendChild(gameCard);
+
+function Game(title, author, hours, played){
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.hours = hours;
+    this.played = played;
     this.info = function(){
-        info = title + ", " + author +", "+ pages + ", "+ read;
+        info = title + "\n" + author + "\n" + hours + "\n" + played;
         return info;
     }
 }
@@ -16,9 +23,25 @@ function addBookToLibrary(){
     author = prompt("Write the author of the book: ");
     pages = prompt("Write how many pages does the book have: ");
     read = prompt("Have you read this book yet?: ");
-    title = new Book(title, author, pages, read);
+    title = new Game(title, author, hours, played);
     myLibrary.push(title);
 }
+
+const game1 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
+const game2 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
+
+for(i=0; i<5; i++){
+    const paragraph = document.createElement('p');
+    gameCard.innerText = game1.info();
+    gameCard.appendChild(paragraph);
+}
+
+
+myLibrary.push(game1);
+myLibrary.push(game2);
+console.log(myLibrary);
+
+
 /*addBookToLibrary();
 console.log(title.info());
 console.log(myLibrary[0]);
