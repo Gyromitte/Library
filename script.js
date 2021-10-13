9b3ef4f261e1f8112569debef4fcbf96d38608a1
Gyromitte <gyromitte@gmail.com>
2021-10-12 20:06:57 -0500

    The button Add game now works fully

--- script.js
+++ script.js
@@ -8,55 +8,44 @@ const addGame = document.getElementById('add-game-button');
 const modal = document.getElementById('add-game-modal');
 const closeButton = document.getElementById('close-button');
 
-function Game(title, author, hours, played){
+const submittButton = document.getElementById('submit-button');
+
+function Game(title, author, completed, played){
     this.title = title;
     this.author = author;
-    this.hours = hours;
+    this.completed = completed;
     this.played = played;
     this.info = function(){
-        info = title + "\n" + author + "\n" + hours + "\n" + played;
+        info = title + "\n" + author + "\n" + completed + "\n" + played;
         return info;
     }
 }
-const game1 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game2 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game3 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game4 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game5 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game6 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game7 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game8 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game9 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game10 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game11= new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game12 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-const game13 = new Game("Hollow knight", "Team cherry", "32 hours", "Played");
-const game14 = new Game("Bornout paradise", "Electronic Arts", "40 hours", "Played");
-
-
-myLibrary.push(game1);
-myLibrary.push(game2, game3, game4, game5, game6, game7, game8, game9,
-    game10, game11, game12, game13, game14);
-console.log(myLibrary);
 
 function addBookToLibrary(){
-    title = prompt("Write the title of the book: ");
-    author = prompt("Write the author of the book: ");
-    pages = prompt("Write how many pages does the book have: ");
-    read = prompt("Have you read this book yet?: ");
-    title = new Game(title, author, hours, played);
+    title = document.getElementById('name').value;
+    author = document.getElementById('developer').value;
+    if(document.getElementById('played').checked){
+        completed = "Played";
+    }
+    if(!document.getElementById('played').checked){
+        completed = "Not played";
+    }
+    if(document.getElementById('completed').checked){
+        played = "Completed";
+    }
+    if(!document.getElementById('completed').checked){
+        played = "Not completed";
+    }
+    title = new Game(title, author, completed, played);
     myLibrary.push(title);
 }
 
 function displayGames(){
-    for(i=0; i < myLibrary.length; i++){
         const gameCard = document.createElement('div');
         gameCard.classList.add('game-card');
-        gameCard.innerText = myLibrary[i].info();
+        gameCard.innerText = myLibrary[myLibrary.length-1].info();
         library.appendChild(gameCard);
-    }
 }
-displayGames();
 
 addGame.addEventListener('click', e=>{
     modal.style.display = "block";
@@ -64,20 +53,16 @@ addGame.addEventListener('click', e=>{
 closeButton.addEventListener('click', e=>{
     modal.style.display = "none";
 });
+submittButton.addEventListener('click', e=>{
+    event.preventDefault();
+    addBookToLibrary();
+    displayGames();
+    modal.style.display = "none";
+    document.forms[0].reset();
+    console.log(myLibrary);
+});
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
 }
-
-
-
-
-
-
-/*addBookToLibrary();
-console.log(title.info());
-console.log(myLibrary[0]);
-addBookToLibrary();
-console.log(myLibrary[1]);
-*/
